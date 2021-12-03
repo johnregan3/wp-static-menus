0293d56fc4d1c2367e523bb090ed8d0a13f01ddc
John Regan <john.m.regan@gmail.com>
2021-12-03 06:09:32 -0600

    Update Tests to use WP_Mock

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -2,41 +2,15 @@
 /**
  * Test Class Mindsize\WPStaticMenus\Plugin
  */
-class PluginTest extends WPStaticMenusTests\TestCase {
 
-	public function test_PluginIsActivated() {
-		$this->assertTrue(
-			class_exists( 'Mindsize\WPStaticMenus\Plugin' ),
-			'❗️WP Static Menus Plugin is not activated.'
-		);
+class PluginTest extends \WP_Mock\Tools\TestCase {
 
-		$this->assertTrue(
-			class_exists( 'WP_Fragment_Object_Cache' ),
-			'❗️WP Fragment Cache Plugin is not activated.'
-		);
-
-		$this->assertTrue(
-			is_a( $this->plugin, 'Mindsize\WPStaticMenus\Plugin' ),
-			'❗️Plugin instance is not loaded in Unit Tests.'
-		);
-
+	public function setUp() : void {
+		\WP_Mock::setUp();
 	}
 
-	public function test_init() {
-
-		$this->assertTrue(
-			is_a( $this->class_settings, 'Mindsize\WPStaticMenus\Settings' ),
-			'❗️Plugin property $settings is not instantiated'
-		);
-
-		$this->assertTrue(
-			is_a( $this->class_cacher, 'Mindsize\WPStaticMenus\Cacher' ),
-			'❗️Plugin property $cacher is not instantiated'
-		);
-
-		$this->assertTrue(
-			is_array( $this->plugin->locations ),
-			'❗️Plugin property $locations is not an array'
-		);
+	public function tearDown() : void {
+		\WP_Mock::tearDown();
 	}
+
 }
